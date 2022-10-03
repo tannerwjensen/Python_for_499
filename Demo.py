46b2fbb3ce024f39fc5fba0c1546e9d0b52d4d11
Tanner Jensen <tannerwjensen@gmail.com>
2022-10-03 17:39:14 -0600

    new

---
+++ Demo.py
@@ -0,0 +1,18 @@
+
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import dfply
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from xgboost import XGBRegressor
+from sklearn.model_selection import train_test_split
+
+
+
+
+
+# %%
